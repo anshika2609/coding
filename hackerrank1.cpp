a55f96e172b8e85edabb45a2d048cea9e57e5ef8
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2020-07-31 11:07:54 -0700

    Add files via upload

--- hackerrank1.cpp
+++ hackerrank1.cpp
@@ -1,24 +1,26 @@
 #include<bits/stdc++.h>
-#define ll long long int
 using namespace std;
 int main()
 {
-    ll n,i;
-    cin>>n;
-    ll arr[n+1];
-    for(i=0;i<n;i++)
-        cin>>arr[i];
-    ll d,m;
-    cin>>d>>m;
-    ll count=0;
-    for(i=0;i<n-m+1;i++)
+    int t;
+    cin>>t;
+    while(t--)
     {
-        ll a=0;
-        for(ll j=i;j<i+m;j++)
-            a+=arr[j];
-        if(a==d)
-            count++;
+        int n,ans=0,i;
+        cin>>n;
+        for(i=0;i<=n;i++)
+        {
+            if(i==0)
+                ans=1;
+            else if(i%2!=0)
+                ans=ans*2;
+            else if(i%2==0 && i!=0)
+                ans+=1;
+        }
+        cout<<ans<<endl;
     }
-    cout<<count<<endl;
+
     return 0;
 }
+
+///// https://www.hackerrank.com/challenges/utopian-tree/problem /////
