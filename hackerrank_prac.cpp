d81fa352c50206ae93c9d713a2edc8b924aadf6d
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2020-08-06 11:23:36 -0700

    Add files via upload

--- hackerrank_prac.cpp
+++ hackerrank_prac.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int n;
-    cin>>n;
-    int p[n+1];
-    for(int i=1;i<=n;i++){
-        int k;
-        cin>>k;
-        p[k]=i;
-    }
-    for(int i=1;i<=n;i++)cout<<p[p[i]]<<endl;
-    return 0;
-}
-//// https://www.hackerrank.com/challenges/permutation-equation/problem ////
+def minimumNumber(n, password):
+    count = 0    
+    if any(i.isdigit() for i in password)==False:
+        count+=1
+    if any(i.islower() for i in password)==False:
+        count+=1
+    if any(i.isupper() for i in password)==False:
+        count+=1
+    if any(i in '!@#$%^&*()-+' for i in password)==False:
+        count+=1
+    return max(count,6-n)
+
+n=int(input())
+st=input()
+ans=minimumNumber(n,st)
+print(ans)
+
+#https://www.hackerrank.com/challenges/strong-password/problem
