73eea9e5f27cb55a48871a7118695e6b48806bd3
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2020-08-06 08:13:36 -0700

    Add files via upload

---
+++ python1.cpp
@@ -0,0 +1,6 @@
+n,d=map(int,input().split())
+li=list(map(int,input().split()))
+p=li[d:]+li[0:d]
+print(*p)
+
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
