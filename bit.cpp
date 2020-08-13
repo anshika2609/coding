3f3a3e98b88028227925e2bae51ed729cce0e1e2
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2020-08-13 10:13:20 -0700

    Add files via upload

---
+++ bit.cpp
@@ -0,0 +1,45 @@
+/* * * * * * * * * * * * * * 		
+#                          *
+# @Author  ANSHIKA AGRAWAL *
+#                          *
+# * * * * * * * * * * * * */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define M(x) (x%MOD + MOD)%MOD
+#define _pb push_back
+#define _mp make_pair
+#define ff first
+#define ss second
+#define s(x) scanf("%lld",&x)
+ 
+ll mul(ll x,ll y)
+{ ll ans=1;
+ 
+  while(y>0)
+  { if(y&1)
+	ans=(ans*x)%MOD;
+	y/=2;
+	x=(x*x)%MOD;
+  }
+ 
+  return ans;
+};
+ 
+/******************************************************************************************************/
+ 
+int main()
+{	
+    int l,r,i,ans=0,j;
+    cin>>l>>r;
+    for(i=l;i<r;i++)
+        for(j=i+1;j<=r;j++)
+            ans=max(ans,i^j);
+
+    cout<<ans<<endl;
+    
+    return 0;
+}
+/// https://www.hackerrank.com/challenges/maximizing-xor/problem
