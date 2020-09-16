37247acdbfaa9285d7c036bec282533d2de297dc
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2020-09-16 10:34:04 -0700

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,69 @@
+/* * * * * * * * * * * * * * 		
+#                          *
+# @Author  ANSHIKA AGRAWAL *
+#                          *
+# * * * * * * * * * * * * */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define M(x) (x%MOD + MOD)%MOD
+#define _pb push_back
+#define _mp make_pair
+#define ff first
+#define ss second
+#define s(x) scanf("%lld",&x)
+ 
+ll mul(ll x,ll y)
+{ ll ans=1;
+ 
+  while(y>0)
+  { if(y&1)
+	ans=(ans*x)%MOD;
+	y/=2;
+	x=(x*x)%MOD;
+  }
+ 
+  return ans;
+};
+ 
+/******************************************************************************************************/
+vector<ll>vv;
+int main()
+{	
+	ll a,b,i;
+	cin>>a>>b;
+	for(i=1;i*i<=a;i++)
+	{
+		if(a%i==0){
+			vv._pb(i);
+			vv._pb(a/i);
+		}
+	}
+	for(i=1;i*i<=b;i++)
+	{
+		if(b%i==0){
+			vv._pb(i);
+			vv._pb(b/i);
+		}
+
+	}
+	ll count=0;
+	map<ll,ll>mp;
+	for(i=0;i<vv.size();i++)
+	{
+		if(mp.find(vv[i]) == mp.end())
+			mp[vv[i]]=1;
+
+		else
+			mp[vv[i]]++;
+	}
+	for(auto &it : mp)
+	{
+		if(it.ss % 2 == 0)
+			count++;
+	}
+	cout<<count<<endl;
+	return 0;
+}
