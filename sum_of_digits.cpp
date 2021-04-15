c8529bfe4638a0358e33c58804afea92e7ecaa89
anshika2609 <47159040+anshika2609@users.noreply.github.com>
2021-04-15 23:58:13 +0530

    Add files via upload

---
+++ sum_of_digits.cpp
@@ -0,0 +1,54 @@
+
+/* * * * * * * * * * * * * * 		
+#                          *
+#  @Author ANSHIKA AGRAWAL *
+#                          *
+# * * * * * * * * * * * * */
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define M(x) (x%MOD + MOD)%MOD
+#define _pb push_back
+#define _mp make_pair
+#define ff first
+#define ss second
+#define s(x) scanf("%lld",&x)
+ 
+ll mul(ll x,ll y)
+{ ll ans=1;
+ 
+  while(y>0)
+  { if(y&1)
+	ans=(ans*x)%MOD;
+	y/=2;
+	x=(x*x)%MOD;
+  }
+ 
+  return ans;
+};
+ 
+/******************************************************************************************************/
+
+int sum_digit(string n, int pos)
+{
+	if(pos==n.size())
+		return 0;
+
+	return n[pos]-'0' + sum_digit(n, pos+1);
+}
+
+int main()
+{	
+	string str;
+	cin>>str;
+	cout<<sum_digit(str, 0);
+	return 0;
+}
+
+
+
+
+/// using recursion of string
