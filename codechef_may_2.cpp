033b823817ad13db8ad6c98395178220a9700d30
anshika2609 <anshikaagrawal2609@gmail.com>
2021-05-08 00:07:08 +0530

    08-05-2021- 0:07:08.81

---
+++ codechef_may_2.cpp
@@ -0,0 +1,52 @@
+
+/* * * * * * * * * * * * * * 		
+#                          *
+#  @Author ANSHIKA AGRAWAL *
+#                          *
+# * * * * * * * * * * * * */
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define M(x) (x%MOD + MOD)%MOD
+#define _pb push_back
+#define _mp make_pair
+#define ff first
+#define ss second
+#define s(x) scanf("%lld",&x)
+ 
+ll mul(ll x,ll y)
+{ ll ans=1;
+ 
+  while(y>0)
+  { if(y&1)
+	ans=(ans*x)%MOD;
+	y/=2;
+	x=(x*x)%MOD;
+  }
+ 
+  return ans;
+};
+ 
+/******************************************************************************************************/
+ 
+int main()
+{	
+
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n,x,k,z;
+		cin>>n>>x>>k;
+		z=abs(x-(n+1));
+		if(z%k==0 || x%k==0)
+			cout<<"YES"<<endl;
+		else
+			cout<<"NO"<<endl;
+		
+	}
+	return 0;
+}
