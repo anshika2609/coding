50b43b740a5a605213a5b5caea31376bd0d37f70
anshika2609 <anshikaagrawal2609@gmail.com>
2021-05-08 23:12:04 +0530

    08-05-2021-23:12:04.04

---
+++ Node_at_distance.cpp
@@ -0,0 +1,32 @@
+void leafsAtDistance(Node *root, vector<Node*> &path, int k, set<Node*> &ans)
+{
+    if(root==NULL)
+        return;
+        
+    path.push_back(root);
+    
+    if(root->left==NULL && root->right==NULL)
+    {
+        int size = path.size();
+        if(size-1-k>=0)
+            {
+                // cout<<root->data<<" "<<size-1-k<<"\n";
+                ans.insert(path[size-1-k]);
+            }
+    } else
+    {
+        leafsAtDistance(root->left, path, k, ans);
+        leafsAtDistance(root->right, path, k, ans);
+    }
+    
+    path.pop_back();
+    return;
+}
+
+int printKDistantfromLeaf(Node* root, int k)
+{
+	vector<Node*> path;
+	set<Node*> ans;
+	leafsAtDistance(root, path, k, ans);
+    return (int)ans.size();	
+}
