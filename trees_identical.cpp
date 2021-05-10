c98a5629298f10aa0d999a3f9a3f9e35b55b3869
anshika2609 <anshikaagrawal2609@gmail.com>
2021-05-10 23:43:00 +0530

    10-05-2021-23:43:00.21

---
+++ trees_identical.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1==NULL && r2==NULL)
+            return 1;
+        if(r1!=NULL && r2!=NULL)
+        {
+            if(r1->data == r2->data && isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right))
+                return 1;
+        }
+        return 0;
+    }
+};
